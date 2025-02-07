e4970e8d7f45b5ab63ffaae17bd279fd6947a119
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-02-07 21:51:42 +0530

    Time: 43 ms (45.85%), Space: 99.5 MB (17.03%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,31 +1,36 @@
-import java.util.*;
-
 class Solution {
+
     public int[] queryResults(int limit, int[][] queries) {
-        Map<Integer, Integer> node = new HashMap<>();
-        Map<Integer, Integer> color = new HashMap<>();
-        int[] ans = new int[queries.length];
+        int n = queries.length;
+        int[] result = new int[n];
+        Map<Integer, Integer> colorMap = new HashMap<>();
+        Map<Integer, Integer> ballMap = new HashMap<>();
 
-        for (int i = 0; i < queries.length; i++) {
-            int x = queries[i][0], y = queries[i][1];
+        // Iterate through queries
+        for (int i = 0; i < n; i++) {
+            // Extract ball label and color from query
+            int ball = queries[i][0];
+            int color = queries[i][1];
 
-            if (node.containsKey(x)) {
-                int prevColor = node.get(x);
-                if (prevColor == y) { 
-                    ans[i] = color.size(); 
-                    continue;
+            // Check if ball is already colored
+            if (ballMap.containsKey(ball)) {
+                // Decrement count of the previous color on the ball
+                int prevColor = ballMap.get(ball);
+                colorMap.put(prevColor, colorMap.get(prevColor) - 1);
+
+                // If there are no balls with previous color left, remove color from color map
+                if (colorMap.get(prevColor) == 0) {
+                    colorMap.remove(prevColor);
                 }
-                if (color.get(prevColor) == 1) 
-                    color.remove(prevColor);
-                else 
-                    color.put(prevColor, color.get(prevColor) - 1);
             }
+            // Set color of ball to the new color
+            ballMap.put(ball, color);
 
-            node.put(x, y);
-            color.put(y, color.getOrDefault(y, 0) + 1);
-            ans[i] = color.size();
+            // Increment the count of the new color
+            colorMap.put(color, colorMap.getOrDefault(color, 0) + 1);
+
+            result[i] = colorMap.size();
         }
-
-        return ans;
+        return result;
     }
 }
