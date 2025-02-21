1c1502cee2ad782987f6cbcf9455bbe945f75572
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-02-21 22:01:35 +0530

    Time: 19 ms (100%), Space: 47 MB (73.85%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -1,33 +1,28 @@
-class FindElements {
 
-    HashSet<Integer> seen;
+
+class FindElements {
+    BitSet recoveredValues;
 
     public FindElements(TreeNode root) {
-        seen = new HashSet<>();
-        bfs(root);
+        root.val = 0;
+        recoveredValues = new BitSet();
+        recoverTree(root);
+    }
+
+    private void recoverTree(TreeNode root) {
+        if (root == null) return;
+        recoveredValues.set(root.val);
+        if (root.left != null) {
+            root.left.val = 2 * root.val + 1;
+            recoverTree(root.left);
+        }
+        if (root.right != null) {
+            root.right.val = 2 * root.val + 2;
+            recoverTree(root.right);
+        }
     }
 
     public boolean find(int target) {
-        return seen.contains(target);
-    }
-
-    private void bfs(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        root.val = 0;
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode currentNode = queue.remove();
-            // visit currentNode by adding its recovered value to the set
-            seen.add(currentNode.val);
-            if (currentNode.left != null) {
-                currentNode.left.val = currentNode.val * 2 + 1;
-                queue.add(currentNode.left);
-            }
-            if (currentNode.right != null) {
-                currentNode.right.val = currentNode.val * 2 + 2;
-                queue.add(currentNode.right);
-            }
-        }
+        return recoveredValues.get(target);
     }
 }
