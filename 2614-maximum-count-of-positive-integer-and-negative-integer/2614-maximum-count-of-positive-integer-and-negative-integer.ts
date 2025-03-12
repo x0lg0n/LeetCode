f92a10880758101e7dc1a73c560e7f8cd45fa343
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-03-12 19:03:33 +0530

    Time: 0 ms (100%), Space: 58.2 MB (26.76%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.ts
@@ -0,0 +1,8 @@
+function maximumCount(nums: number[]): number {
+    let pos = 0, neg = 0;
+        for (let num of nums) {
+            if (num > 0) pos++;
+            else if (num < 0) neg++;
+        }
+        return Math.max(pos, neg);
+};
