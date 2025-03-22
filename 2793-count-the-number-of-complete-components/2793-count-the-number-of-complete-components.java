831814c34afc7d20719d5a7f88df888051c73784
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-03-22 13:04:30 +0530

    Time: 14 ms (39.21%), Space: 45.3 MB (56.68%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,43 +1,64 @@
-import java.util.*;
-
 class Solution {
-    private Map<Integer, List<Integer>> graph = new HashMap<>();
+    int[] parent;
+    int[] rank;
 
     public int countCompleteComponents(int n, int[][] edges) {
-        for (int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
-        }
-
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
+        parent = new int[n];
+        rank = new int[n];
 
         for (int i = 0; i < n; i++) {
-            if (!visited.contains(i)) {
-                Set<Integer> component = new HashSet<>();
-                dfs(i, component, visited);
-                if (isCompleteComponent(component)) count++;
+            parent[i] = i;
+        }
+
+        for (int[] edge : edges) {
+            union(edge[0], edge[1]);
+        }
+
+        Map<Integer, Set<Integer>> componentVertices = new HashMap<>();
+        Map<Integer, Integer> componentEdges = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int root = find(i);
+            componentVertices.computeIfAbsent(root, k -> new HashSet<>()).add(i);
+        }
+
+        for (int[] edge : edges) {
+            int root = find(edge[0]);
+            componentEdges.put(root, componentEdges.getOrDefault(root, 0) + 1);
+        }
+
+        int completeCount = 0;
+        for (int root : componentVertices.keySet()) {
+            int numVertices = componentVertices.get(root).size();
+            int expectedEdges = numVertices * (numVertices - 1) / 2;
+
+            if (componentEdges.getOrDefault(root, 0) == expectedEdges) {
+                completeCount++;
             }
         }
-        return count;
+
+        return completeCount;
     }
 
-    private void dfs(int node, Set<Integer> component, Set<Integer> visited) {
-        component.add(node);
-        visited.add(node);
-        for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
-            if (!visited.contains(neighbor)) {
-                dfs(neighbor, component, visited);
-            }
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
         }
+        return parent[x];
     }
 
-    private boolean isCompleteComponent(Set<Integer> component) {
-        for (int node : component) {
-            if (graph.getOrDefault(node, new ArrayList<>()).size() != component.size() - 1) {
-                return false;
-            }
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
         }
-        return true;
     }
 }
