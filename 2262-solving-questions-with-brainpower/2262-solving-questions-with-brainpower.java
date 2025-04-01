b5d230919730686b196432c4c27e40c8da3951ee
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-04-01 15:25:47 +0530

    Time: 5 ms (96.96%), Space: 108.3 MB (65.18%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,12 +1,19 @@
 class Solution {
-    public long mostPoints(int[][] ques) {
-        int n = ques.length;
-        Long[] memo = new Long[n];
-        return check(ques, 0, memo);
-    }
-    private long check(int[][] ques, int i, Long[] memo) {
-        if(i >= ques.length) return 0;
-        if(memo[i] != null) return memo[i];
-        return memo[i] = Math.max(check(ques, i + 1, memo), ques[i][0] + check(ques, i + ques[i][1] + 1, memo));
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int points = questions[i][0];
+            int brainpower = questions[i][1];
+            int next = i + brainpower + 1;
+
+            long take = points + (next < n ? dp[next] : 0);
+            long skip = dp[i + 1];
+
+            dp[i] = Math.max(take, skip);
+        }
+
+        return dp[0];
     }
 }
