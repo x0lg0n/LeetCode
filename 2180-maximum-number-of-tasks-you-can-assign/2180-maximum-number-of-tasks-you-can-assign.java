582114c74b4290ca9151763713cb96b0722ecd56
Siddhartha Kunwar <kumarsiddharthakain@gmail.com>
2025-05-01 22:18:07 +0530

    Time: 1570 ms (28.98%), Space: 55.6 MB (65.31%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -1,61 +1,44 @@
 class Solution {
-
-    public int maxTaskAssign(
-        int[] tasks,
-        int[] workers,
-        int pills,
-        int strength
-    ) {
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        int left = 0, right = Math.min(tasks.length, workers.length);
         Arrays.sort(tasks);
         Arrays.sort(workers);
-        int n = tasks.length, m = workers.length;
-        int left = 1, right = Math.min(m, n), ans = 0;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (check(tasks, workers, pills, strength, mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            int usedPills = 0;
+            TreeMap<Integer, Integer> avail = new TreeMap<>();
+            for (int i = workers.length - mid; i < workers.length; ++i)
+                avail.put(workers[i], avail.getOrDefault(workers[i], 0) + 1);
+
+            boolean canAssign = true;
+            for (int i = mid - 1; i >= 0; --i) {
+                int t = tasks[i];
+                int w = avail.lastKey();
+                if (w >= t) {
+                    decrement(avail, w);
+                } else {
+                    Integer key = avail.ceilingKey(t - strength);
+                    if (key == null || ++usedPills > pills) {
+                        canAssign = false;
+                        break;
+                    }
+                    decrement(avail, key);
+                }
             }
+
+            if (canAssign)
+                left = mid;
+            else
+                right = mid - 1;
         }
-        return ans;
+
+        return left;
     }
 
-    private boolean check(
-        int[] tasks,
-        int[] workers,
-        int pills,
-        int strength,
-        int mid
-    ) {
-        int p = pills;
-        TreeMap<Integer, Integer> ws = new TreeMap<>();
-        for (int i = workers.length - mid; i < workers.length; ++i) {
-            ws.put(workers[i], ws.getOrDefault(workers[i], 0) + 1);
-        }
-        for (int i = mid - 1; i >= 0; --i) {
-            Integer key = ws.lastKey();
-            if (key >= tasks[i]) {
-                ws.put(key, ws.get(key) - 1);
-                if (ws.get(key) == 0) {
-                    ws.remove(key);
-                }
-            } else {
-                if (p == 0) {
-                    return false;
-                }
-                key = ws.ceilingKey(tasks[i] - strength);
-                if (key == null) {
-                    return false;
-                }
-                ws.put(key, ws.get(key) - 1);
-                if (ws.get(key) == 0) {
-                    ws.remove(key);
-                }
-                --p;
-            }
-        }
-        return true;
+    private void decrement(TreeMap<Integer, Integer> m, int k) {
+        int c = m.get(k);
+        if (c == 1) m.remove(k);
+        else m.put(k, c - 1);
     }
 }
